be9c9bd89ce88245fe902f1e1ee4544bf8c7de73
zprintltd <idara.zprint@gmail.com>
2025-12-29 22:19:40 +0300

    Update bulk.js

--- bulk.js
+++ bulk.js
@@ -1,18 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2865
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ZPRINT v2.7 – Bulk Preview</title>
+</head>
+<body>
 
-\f0\fs24 \cf0 const SCRIPT_URL = ‘https://script.google.com/macros/s/AKfycbwkE9WHkiSqjMZBZsUYIF0BH3ivgbevnYzjqFzvCo0d-gBkje-_-fCwZmOs8vOtAxhO/exec’;\
-\
-function bulkPrint()\{\
-  fetch(SCRIPT_URL+"?action=bulkPrint")\
-    .then(r=>r.json())\
-    .then(d=>\{\
-      if(d.docUrl)\{\
-        window.open(d.docUrl,"_blank");\
-      \}\
-    \});\
-\}}
+<h2>Bulk Preview</h2>
+<button onclick="bulkPrint()">Generate Preview</button>
+
+<script src="bulk.js?v=2.7.0"></script>
+</body>
+</html>
